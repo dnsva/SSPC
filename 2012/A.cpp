d3a4492f5cebf7e57afb9b17bb65bafa4cfd6478
Anna <peachtiger0@gmail.com>
2022-11-29 22:28:46 -0500

    upload

--- 2012/A.cpp
+++ 2012/A.cpp
@@ -1,7 +1,30 @@
 
 #include <iostream>
+#include <vector> 
 
 using namespace std;
+
+void print_commas(double avg){
+
+    //First slit int up into digits then loop
+    vector<int>digits;
+    int avg_int = avg; //cast to int 
+    while(avg_int > 0){
+        int digit = avg_int%10;
+        avg_int /= 10;
+        digits.push_back(digit);
+    }
+    reverse(digits.begin(), digits.end());
+
+    //Cout
+    for(int i = 0; i<digits.size(); ++i){
+        cout<<digits[i];
+        if(i%3 == 0 && i != digits.size()-1 && !((i == 0) && digits.size() <= 3)){
+            cout<<",";
+        }
+    }
+    cout<<"."<<int((avg - int(avg))*100.0);
+}
 
 void solve(){
     double total; 
@@ -10,17 +33,18 @@ void solve(){
         cin>>month_val;
         total += month_val;
     }
-
-    for(int i = 100; i*3 < total; i*=3){
-        //!!! IN book
-    }
-
+    double avg = total / 12;
+    cout<<"$";
+    print_commas(avg);
+    cout<<" $";
+    print_commas(avg*0.1);
+    cout<<"\n";
 }
 
 int main(){
     int N;
     cin>>N;
-    for(int i = 2; i<=N; ++i){
+    for(int i = 1; i<=N; ++i){
         cout<<i<<" ";
         solve();
     }
